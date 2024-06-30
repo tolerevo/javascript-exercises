4c3c7a57eccdf835ea27a3550b60da321de76382
Diego Tolentino <114973390+tolerevo@users.noreply.github.com>
2024-06-29 21:47:53 -0600

    did leapyear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  const isYearDivisibleByFour = year % 4 === 0;
+  const isYearDivisibleByFourHundred = year % 400 === 0;
+  const isYearDivisibleByHundred = year % 100 === 0;
+  if (isYearDivisibleByFour &&!isYearDivisibleByHundred || isYearDivisibleByFourHundred) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
