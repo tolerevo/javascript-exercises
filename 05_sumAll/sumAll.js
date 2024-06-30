123c5e5f22c18c3cbf8553521aefa93db55ec89a
Diego Tolentino <114973390+tolerevo@users.noreply.github.com>
2024-06-29 19:01:20 -0600

    did sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
+  return ((Math.abs(a - b) + 1) * (a + b)) / 2;
 };
 
 // Do not edit below this line
